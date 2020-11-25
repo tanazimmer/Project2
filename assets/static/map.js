4b1171096b6fce7ab1222ae566cb9b08c70a22f4
tanazimmer <68484974+tanazimmer@users.noreply.github.com>
2020-11-24 20:04:26 -0500

    trying

--- assets/static/map.js
+++ assets/static/map.js
@@ -1,9 +1,13 @@
+// Set a cross-site cookie for third-party contexts
+//gtag('config', 'OUR_GA_ID', {cookie_flags: 'SameSite=None;Secure'});
+
+
 // Creating map object
 var myMap = L.map("map", {
   center: [40.7, -73.95],
   zoom: 11
 });
-
+ console.log("test")
 // Adding tile layer to the map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -13,3 +17,4 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   id: "mapbox/streets-v11",
   accessToken: API_KEY
 }).addTo(myMap);
+console.log(myMap)
